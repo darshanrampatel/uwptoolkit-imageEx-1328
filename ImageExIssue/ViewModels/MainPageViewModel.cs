b63a0ed8f620df335f175a3b90c8039b23db65c3
Darshan Patel <darshan@firststeps.software>
2017-07-22 21:00:18 +0100

    Initial add

---
+++ ImageExIssue/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Windows.UI.Xaml.Navigation;
+
+namespace ImageExIssue.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public void LoadItems()
+        {
+            var list = new List<Picture>();
+            for (int i = 0; i < 200; i++)
+            {
+                var newItem = new Picture(i);
+                list.Add(newItem);
+            }
+            Pictures = list;
+        }
+
+        private List<Picture> _Pictures = new List<Picture>();
+        public List<Picture> Pictures
+        {
+            get => _Pictures;
+            set
+            {
+                Set(ref _Pictures, value);
+            }
+        }
+    }
+
+    public class Picture
+    {
+        public Picture(int i)
+        {
+            ID = i + 1;
+            URL = $"https://unsplash.it/128?image={i}";
+        }
+
+        public int ID { get; set; }
+        public string URL { get; set; }
+    }
+}
---
+++ ImageExIssue/Views/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Toolkit.Uwp.UI.Controls;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace ImageExIssue.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+
+        private void Image_ImageFailed(object sender, Windows.UI.Xaml.ExceptionRoutedEventArgs e)
+        {
+            Image img = sender as Image;
+            string fallbackImage = "ms-appx:///Assets/StoreLogo.png";
+            img.Source = new BitmapImage(new Uri(fallbackImage));
+        }
+
+        private void Image_ImageExFailed(object sender, Microsoft.Toolkit.Uwp.UI.Controls.ImageExFailedEventArgs e)
+        {
+            ImageEx img = sender as ImageEx;
+            string fallbackImage = "ms-appx:///Assets/StoreLogo.png";
+            img.Source = fallbackImage;
+        }
+    }
+}
